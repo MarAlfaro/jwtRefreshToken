9e4c5c950ca516a45ec328b00437d2b8f50c1a50
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-06-20 17:16:05 -0600

    prueba

--- index.js
+++ index.js
@@ -42,5 +42,6 @@ app.use('/api/public', publicRoutes);  // Ruta pública
 app.use('/api/auth', authRoutes);      // Ruta de procesos
 app.use('/api/private', privateRoutes); // Ruta privada
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
+const PORT = 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
