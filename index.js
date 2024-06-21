b06d8989f29e9a8513766ac46ec27d269bd5f9de
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-06-20 18:26:31 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// const express = require('express');
-// const connectDB = require('./config/db');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const publicRoutes = require('./routes/publicRoutes');
-// const privateRoutes = require('./routes/privateRoutes');
-// const authRoutes = require('./routes/authRoutes');
-
-// const app = express();
-// app.use(express.json());
-
-// connectDB();
-
-// app.use('/api/public', publicRoutes);  //ruta publica.
-// app.use('/api/auth', authRoutes); //ruta de procesos.
-// app.use('/api/private', privateRoutes);//ruta privada.
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
